85a8d3a2ffe0ec79285ed58bbe14625487cc6b47
Ecasept <76942454+Ecasept@users.noreply.github.com>
2024-12-08 01:29:35 +0100

    add overall benchmark

--- 2024/test/benchmark.py
+++ 2024/test/benchmark.py
@@ -1,13 +1,18 @@
 import os
 import re
 import subprocess as sp
+import time
 
 # Change the directory to the root of the project
 os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 def compile_all():
+    print("\033[1mCompiling all solutions\033[0m")
+    start = time.time()
     os.system("make all")
+    end = time.time()
+    print(f"Total time taken: \033[1m{end - start:.2f}s\033[0m")
     print()
 
 
@@ -33,5 +38,36 @@ def benchmark_all():
             os.chdir("..")
 
 
+def benchmark_single(day: int):
+    start = time.time()
+    sp.run(args=[f"../build/solution{day}"], stdout=sp.DEVNULL)
+    end = time.time()
+    return end - start
+
+
+def benchmark_overall():
+    print("\033[1mRunning overall benchmark\033[0m")
+    total_time = 0
+    dir_count = 0
+    dirs = os.listdir(".")
+    for dir in dirs:
+        if os.path.isdir(dir):
+            # is of format "Day <num>"
+            if not re.match(r"Day \d+", dir):
+                continue
+            # get the day number
+            day = int(re.search(r"\d+", dir).group())
+
+            os.chdir(dir)
+            total_time += benchmark_single(day)
+            os.chdir("..")
+            dir_count += 1
+    print(
+        f"""Total time taken: \033[1m{total_time:.3f}s\033[0m for \033[1m{
+            dir_count}\033[0m problems (avg: \033[1m{total_time / dir_count:.3f}s\033[0m)"""
+    )
+
+
 compile_all()
 benchmark_all()
+benchmark_overall()
