239086e4a74614c45196f54f40073ed6357156d7
Ecasept <76942454+Ecasept@users.noreply.github.com>
2024-12-08 14:03:14 +0100

    undo optimization because the input is small enough that the unoptimized version is faster :(

--- 2024/Day 8/solution.c
+++ 2024/Day 8/solution.c
@@ -2,56 +2,37 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-// first antenna is at index 0, second at index 1, etc.
-struct AntennaMap {
-	int *antenna_indices;
-	size_t antenna_count;
-};
-
-// grups all antennas with the same frequency
-struct FrequenciesMap {
-	struct AntennaMap frequencies[256];
-};
-
-void parse_antennas(char *data, size_t columns, size_t rows,
-					struct FrequenciesMap *map) {
-	for (size_t i = 0; i < (columns * rows); i++) {
-		if (data[i] != '.' && data[i] != '#') {
-			size_t antenna_count =
-				map->frequencies[(unsigned char)data[i]].antenna_count;
-			if (antenna_count == 0) {
-				map->frequencies[(unsigned char)data[i]].antenna_indices =
-					malloc(sizeof(int) * (columns * rows));
+size_t get_antenna_count(char *data, size_t columns, size_t rows) {
+	size_t count = 0;
+	for (size_t i = 0; i < columns; i++) {
+		for (size_t j = 0; j < rows; j++) {
+			if (data[i * rows + j] != '.' && data[i * rows + j] != '#') {
+				count++;
 			}
-			map->frequencies[(unsigned char)data[i]]
-				.antenna_indices[antenna_count] = i;
-			map->frequencies[(unsigned char)data[i]].antenna_count++;
 		}
 	}
+	return count;
 }
 
-// check if nth order antinodes exist, if yes then set them and return true,
-// otherwise return false
-bool calc_antinodes(int order, int a1_x, int a1_y, int a2_x, int a2_y, int dx,
-					int dy, int columns, int rows, bool *antinodes) {
-	bool did_exist = false;
-	int new_x1 = a1_x + order * dx;
-	int new_y1 = a1_y + order * dy;
-	int new_x2 = a2_x - order * dx;
-	int new_y2 = a2_y - order * dy;
-	if (new_x1 >= 0 && new_y1 >= 0 && new_x1 < columns && new_y1 < rows) {
-		int antinode1_index = new_y1 * columns + new_x1;
-		antinodes[antinode1_index] = true;
-		did_exist = true;
+size_t *get_antennas(char *data, size_t columns, size_t rows, size_t count,
+					 size_t *length) {
+	size_t *antennas = malloc(count * sizeof(size_t));
+	*length = 0;
+	for (size_t i = 0; i < columns; i++) {
+		for (size_t j = 0; j < rows; j++) {
+			size_t index = i * rows + j;
+			if (data[index] != '.' && data[i * rows + j] != '#') {
+				antennas[*length] = index;
+				(*length)++;
+			}
+		}
 	}
-	if (new_x2 >= 0 && new_y2 >= 0 && new_x2 < columns && new_y2 < rows) {
-		int antinode2_index = new_y2 * columns + new_x2;
-		antinodes[antinode2_index] = true;
-		did_exist = true;
+	if (count != *length) {
+		printf("Error: count != length\n");
+		exit(1);
 	}
-	return did_exist;
+	return antennas;
 }
 
 llu part1() {
@@ -61,45 +42,82 @@ llu part1() {
 	size_t rows;
 	data = remove_newlines_and_null_terminator(data, &fsize, &columns, &rows);
 
-	struct FrequenciesMap fmap = {0};
-	parse_antennas(data, columns, rows, &fmap);
+	size_t count = get_antenna_count(data, columns, rows);
+	size_t antenna_count;
+	size_t *antennas = get_antennas(data, columns, rows, count, &antenna_count);
 
-	bool antinodes[fsize];
-	memset(antinodes, 0, fsize * sizeof(bool));
+	bool *antinodes = malloc(sizeof(bool) * fsize);
+	for (size_t i = 0; i < fsize; i++) {
+		antinodes[i] = false;
+	}
 
-	for (int frequency = 0; frequency < 256; frequency++) {
-		struct AntennaMap amap = fmap.frequencies[frequency];
-		for (size_t a1_i = 0; a1_i < amap.antenna_count; a1_i++) {
-			for (size_t a2_i = a1_i + 1; a2_i < amap.antenna_count; a2_i++) {
-				size_t a1 = amap.antenna_indices[a1_i];
-				size_t a2 = amap.antenna_indices[a2_i];
+	for (size_t a1_i = 0; a1_i < antenna_count; a1_i++) {
+		for (size_t a2_i = a1_i + 1; a2_i < antenna_count; a2_i++) {
+			size_t a1 = antennas[a1_i];
+			size_t a2 = antennas[a2_i];
+			if (data[a1] != data[a2]) {
+				// antennas don't have the same frequence
+				continue;
+			}
+			size_t a1_y = a1 / columns;
+			size_t a1_x = a1 % columns;
 
-				size_t a1_y = a1 / columns;
-				size_t a1_x = a1 % columns;
+			size_t a2_y = a2 / columns;
+			size_t a2_x = a2 % columns;
 
-				size_t a2_y = a2 / columns;
-				size_t a2_x = a2 % columns;
+			int dx = a2_x - a1_x;
+			int dy = a2_y - a1_y;
 
-				int dx = a2_x - a1_x;
-				int dy = a2_y - a1_y;
-
-				calc_antinodes(2, a1_x, a1_y, a2_x, a2_y, dx, dy, columns, rows,
-							   antinodes);
+			// first order antinodes (where antenna is twice as far away)
+			if ((int)a1_y - dy >= 0 && (int)a1_x - dx >= 0 &&
+				a1_y - dy < rows && a1_x - dx < columns) {
+				int antinode1_index = (a1_y - dy) * columns + a1_x - dx;
+				antinodes[antinode1_index] = true;
+			}
+			if ((int)a2_y + dy >= 0 && (int)a2_x + dx >= 0 &&
+				a2_y + dy < rows && a2_x + dx < columns) {
+				int antinode2_index = (a2_y + dy) * columns + a2_x + dx;
+				antinodes[antinode2_index] = true;
 			}
 		}
 	}
 	llu antinode_count = 0;
 	for (size_t i = 0; i < fsize; i++) {
 		antinode_count += antinodes[i];
+		// if (antinodes[i]) {
+		// 	printf("%lu\n", i);
+		// }
 	}
 
+	free(antinodes);
+	free(antennas);
 	free(data);
-	for (int frequency = 0; frequency < 256; frequency++) {
-		free(fmap.frequencies[frequency].antenna_indices);
-	}
 	return antinode_count;
 }
 
+// check if nth order antinodes exist, if yes then set them and return true,
+// otherwise return false
+bool calc_antinodes(int order, size_t a1_x, size_t a1_y, size_t a2_x,
+					size_t a2_y, size_t dx, size_t dy, size_t columns,
+					size_t rows, bool *antinodes) {
+	bool did_exist = false;
+	if ((int)a1_y - (int)(order * dy) >= 0 &&
+		(int)a1_x - (int)(order * dx) >= 0 && a1_y - (order * dy) < rows &&
+		a1_x - (order * dx) < columns) {
+		int antinode1_index =
+			(a1_y - order * dy) * columns + a1_x - (int)order * dx;
+		antinodes[antinode1_index] = true;
+		did_exist = true;
+	}
+	if ((int)a2_y + (int)(order * dy) >= 0 &&
+		(int)a2_x + (int)(order * dx) >= 0 && a2_y + order * dy < rows &&
+		a2_x + order * dx < columns) {
+		int antinode2_index = (a2_y + order * dy) * columns + a2_x + order * dx;
+		antinodes[antinode2_index] = true;
+		did_exist = true;
+	}
+	return did_exist;
+}
 llu part2() {
 	size_t fsize;
 	char *data = load_file("input.txt", &fsize);
@@ -107,38 +125,38 @@ llu part2() {
 	size_t rows;
 	data = remove_newlines_and_null_terminator(data, &fsize, &columns, &rows);
 
-	struct FrequenciesMap fmap = {0};
-	parse_antennas(data, columns, rows, &fmap);
+	size_t count = get_antenna_count(data, columns, rows);
+	size_t antenna_count;
+	size_t *antennas = get_antennas(data, columns, rows, count, &antenna_count);
 
 	bool *antinodes = malloc(sizeof(bool) * fsize);
 	for (size_t i = 0; i < fsize; i++) {
 		antinodes[i] = false;
 	}
 
-	for (int frequency = 0; frequency < 256; frequency++) {
-		struct AntennaMap amap = fmap.frequencies[frequency];
-		for (size_t a1_i = 0; a1_i < amap.antenna_count; a1_i++) {
-			for (size_t a2_i = a1_i + 1; a2_i < amap.antenna_count; a2_i++) {
-				size_t a1 = amap.antenna_indices[a1_i];
-				size_t a2 = amap.antenna_indices[a2_i];
+	for (size_t a1_i = 0; a1_i < antenna_count; a1_i++) {
+		for (size_t a2_i = a1_i + 1; a2_i < antenna_count; a2_i++) {
+			size_t a1 = antennas[a1_i];
+			size_t a2 = antennas[a2_i];
+			if (data[a1] != data[a2]) {
+				// antennas don't have the same frequence
+				continue;
+			}
+			size_t a1_y = a1 / columns;
+			size_t a1_x = a1 % columns;
 
-				size_t a1_y = a1 / columns;
-				size_t a1_x = a1 % columns;
+			size_t a2_y = a2 / columns;
+			size_t a2_x = a2 % columns;
 
-				size_t a2_y = a2 / columns;
-				size_t a2_x = a2 % columns;
+			int dx = a2_x - a1_x;
+			int dy = a2_y - a1_y;
 
-				int dx = a2_x - a1_x;
-				int dy = a2_y - a1_y;
-
-				int order = 0;
-				bool next_exists = true;
-				while (next_exists) {
-					next_exists =
-						calc_antinodes(order, a1_x, a1_y, a2_x, a2_y, dx, dy,
-									   columns, rows, antinodes);
-					order++;
-				}
+			int order = 0;
+			bool next_exists = true;
+			while (next_exists) {
+				next_exists = calc_antinodes(order, a1_x, a1_y, a2_x, a2_y, dx,
+											 dy, columns, rows, antinodes);
+				order++;
 			}
 		}
 	}
@@ -148,9 +166,7 @@ llu part2() {
 	}
 
 	free(antinodes);
+	free(antennas);
 	free(data);
-	for (int frequency = 0; frequency < 256; frequency++) {
-		free(fmap.frequencies[frequency].antenna_indices);
-	}
 	return antinode_count;
 }
