583cb61898988493d866581949be0f144ee3fd5d
Ecasept <76942454+Ecasept@users.noreply.github.com>
2025-12-05 07:29:38 +0000

    feat: update setup script to create solution file instead of folder

--- 2025/setup.sh
+++ 2025/setup.sh
@@ -1,9 +1,9 @@
 day=$1
 year=2025
-folder="src/d$day"
+file="src/days/d$day.rs"
 inputs_folder="inputs/d$day"
-if [ -d "$folder" ]; then
-	echo "Solution folder already exists!"
+if [ -f "$file" ]; then
+	echo "Solution file already exists!"
 	exit 1
 fi
 if [ -d "$inputs_folder" ]; then
@@ -11,17 +11,16 @@ if [ -d "$inputs_folder" ]; then
 	exit 1
 fi
 
-mkdir "$folder"
 mkdir "$inputs_folder"
-echo "Created folders"
+echo "Created inputs folder"
 source ./.env
 curl -o "$inputs_folder/input.txt" -H "Cookie: session=$SESSION_ID" "https://adventofcode.com/$year/day/$1/input"
 echo "Downloaded input"
 
 
-cat > "$folder/solution.rs" << END
+cat > "$file" << END
 END
-codium "$folder/solution.rs"
+codium "$file"
 echo "Created and opened solution file"
 
 touch "$inputs_folder/ex1.txt"
