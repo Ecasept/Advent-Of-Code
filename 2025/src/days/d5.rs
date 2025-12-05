4940fec461c56de1385e439d64d0deb6b0043cac
Ecasept <76942454+Ecasept@users.noreply.github.com>
2025-12-05 08:06:21 +0000

    feat: solve day 5

---
+++ 2025/src/days/d5.rs
@@ -0,0 +1,104 @@
+use std::io::BufRead;
+
+use aoc_macros::aoc;
+
+use crate::utils;
+
+#[derive(PartialEq, Eq)]
+enum ParsingState {
+    Ranges,
+    Values
+}
+
+trait Range {
+    fn is_inside(&self, num: u64) -> bool;
+}
+impl Range for (u64, u64) {
+    fn is_inside(&self, num: u64) -> bool {
+        num >= self.0 && num <= self.1
+    }
+}
+
+
+#[aoc(5, 1)]
+fn p1() -> Result<u64, String> {
+    let mut ranges = vec![];
+    let mut sum = 0;
+    let mut parsing_state = ParsingState::Ranges;
+    for line in utils::get_i(5)?.lines() {
+        let line = line.unwrap();
+        if line == "" {
+            parsing_state = ParsingState::Values;
+            continue;
+        }
+        match parsing_state {
+            ParsingState::Ranges => {
+                let (x, y) = line.split_once("-").unwrap();
+                let x = x.parse::<u64>().unwrap();
+                let y = y.parse::<u64>().unwrap();
+                ranges.push((x,y));
+            }
+            ParsingState::Values => {
+                let num = line.parse::<u64>().unwrap();
+                if ranges.iter().any(|r| r.is_inside(num)) {
+                    sum += 1;
+                }
+            }
+        }
+    }
+
+    Ok(sum)
+}
+
+#[aoc(5, 2)]
+fn p2() -> Result<u64, String> {
+    let mut ranges = vec![];
+    let mut sum = 0;
+    for line in utils::get_i(5)?.lines() {
+        let line = line.unwrap();
+        if line == "" {
+            break;
+        }
+        let (x, y) = line.split_once("-").unwrap();
+        let mut x = x.parse::<u64>().unwrap();
+        let mut y = y.parse::<u64>().unwrap();
+
+        let mut i = 0;
+        loop {
+            if i == ranges.len() {
+                // Searched all ranges
+                ranges.push((x,y));
+                break
+            }
+            let range: (u64, u64) = ranges[i];
+            let x_inside = range.is_inside(x);
+            let y_inside = range.is_inside(y);
+            if x_inside && y_inside {
+                // This range is inside another range skip it
+                break;
+            } else if !x_inside && !y_inside {
+                if (x, y).is_inside(range.0) && (x, y).is_inside(range.1) {
+                    // `range` is inside of this range, we can remove it
+                    ranges.remove(i);
+                } else {
+                    // Ranges share no overlap
+                    i+=1;
+                }
+            } else if x_inside && !y_inside {
+                // Extend current range
+                ranges.remove(i);
+                x = range.0;
+            } else if !x_inside && y_inside {
+                // Extend current range
+                ranges.remove(i);
+                y = range.1;
+            }
+        }
+    }
+
+    for range in ranges {
+        sum += range.1 - range.0 + 1;
+    }
+
+    Ok(sum)
+}
